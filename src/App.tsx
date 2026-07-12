7a931d214aa7cfba078669f3c82270fbfbd57502
Joseph25 <zaithangmawiaus@gmail.com>
2026-07-12 22:19:46 +0800

    update button cetering

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,27 @@ import diceIcon from "./assets/images/icon-dice.svg"
 import divider from "./assets/images/pattern-divider-desktop.svg"
 
 function App() {
-    const [advice, setAdvice] = useState<string>(
-    "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-  );
-const [loading, setLoading] = useState<boolean>(false);
+          const [advice, setAdvice] = useState<string>(
+          "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+        );
+        const [loading, setLoading] = useState<boolean>(false);
 
 
- async function fetchAdvice(): Promise<void> {
-  setLoading(true);
+        async function fetchAdvice(): Promise<void> {
+          setLoading(true);
 
-  try {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
+            try {
+              const res = await fetch("https://api.adviceslip.com/advice");
+              const data = await res.json();
 
-    console.log(data.slip.advice);
-    setAdvice(data.slip.advice);
-  } catch (error) {
-    console.log(error);
-  } finally {
-    setLoading(false);
-  }
-}
+              console.log(data.slip.advice);
+              setAdvice(data.slip.advice);
+            } catch (error) {
+              console.log(error);
+            } finally {
+              setLoading(false);
+            }
+        }
 
   return (
   
@@ -38,17 +38,16 @@ const [loading, setLoading] = useState<boolean>(false);
                 </p>
               </div>
           
-          <div className='pb-10 pt-10 flex justify-center'>
-              <img  src={divider}></img>
-    
-          </div>
-          <div className='flex justify-center '>
-            <button onClick={fetchAdvice} className='hover:cursor-pointer absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-1/2  flex justify-center 
-            items-center w-17 h-17 rounded-full bg-[#76FDA8]'>
- 
+              <div className='pb-10 pt-10 mb-8 flex justify-center'>
+                  <img  src={divider}></img>
+        
+              </div>
+         
+            <button onClick={fetchAdvice}  className='hover:cursor-pointer absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2  flex justify-center 
+                            items-center w-17 h-17 rounded-full bg-[#76FDA8]'> 
                 <img className='w-8 h-8'src={diceIcon}></img>
             </button>
-          </div>
+        
         </div>
     </div>
     
